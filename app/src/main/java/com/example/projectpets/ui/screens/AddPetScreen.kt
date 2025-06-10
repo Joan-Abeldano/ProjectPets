dcfeaa253de5f989f3abaf0227f7a5330128c1c8
Joan-Abeldano <joanmabeldano@gmail.com>
2025-06-10 14:01:50 -0600

    AddPetScreen v2: select date dialog added

--- app/src/main/java/com/example/projectpets/ui/screens/AddPetScreen.kt
+++ app/src/main/java/com/example/projectpets/ui/screens/AddPetScreen.kt
@@ -1,23 +1,21 @@
 package com.example.projectpets.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,20 +24,24 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.projectpets.models.Pet
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -108,8 +110,10 @@ fun AddPetScreen() {
 fun ScrollContent(innerPadding: PaddingValues) {
     var petName by remember { mutableStateOf("") }
     var petDescription by remember { mutableStateOf("") }
-    var petBirthDate by remember { mutableStateOf("") }
     var petType by remember { mutableStateOf("") }
+    var showModalInput by remember { mutableStateOf(false) }
+    var petBirthDate by remember { mutableStateOf<Long?>(null) }
+    val formattedDate = remember(petBirthDate) { formatDate(petBirthDate) }
 
     LazyColumn(
         modifier = Modifier
@@ -150,14 +154,16 @@ fun ScrollContent(innerPadding: PaddingValues) {
                 style = MaterialTheme.typography.headlineMedium,
                 color = MaterialTheme.colorScheme.primary
             )
-            OutlinedTextField(
-                value = petBirthDate,
-                onValueChange = { petBirthDate = it },
-                label = { Text("Fecha de Nacimiento", color = MaterialTheme.colorScheme.primary) },
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-                    .fillMaxWidth()
-            )
+            Button(
+                onClick = { showModalInput = true },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+            ) {
+                if (petBirthDate != null) {
+                    Text(formattedDate)
+                } else {
+                    Text("Sin fecha seleccionada", color = MaterialTheme.colorScheme.surface)
+                }
+            }
             Text(
                 text = "Tipo",
                 style = MaterialTheme.typography.headlineMedium,
@@ -173,4 +179,54 @@ fun ScrollContent(innerPadding: PaddingValues) {
             )
         }
     }
+    if (showModalInput) {
+        DatePickerModalInput(
+            onDateSelected = {
+                petBirthDate = it
+                showModalInput = false
+            },
+            onDismiss = { showModalInput = false }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModalInput(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+fun formatDate(millis: Long?): String {
+    if (millis == null) return "Sin fecha seleccionada"
+    val date = Date(millis)
+    val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    return format.format(date)
+}
+
+@Preview
+@Composable
+fun AddPetScreenPreview() {
+    AddPetScreen()
 }
